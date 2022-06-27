4762a7c293c80594c3e81823e6a5e67ff5ac5e74
Nicholas Morrow <njmorrow95@gmail.com>
2022-06-26 19:18:33 -0500

    Move files to native-client.

--- native-client/App.tsx
+++ native-client/App.tsx
@@ -75,8 +75,8 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>Whatever I want!</Text> to
+            change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
